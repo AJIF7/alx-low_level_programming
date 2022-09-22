9a996add1d4e25588ef707811276838ba712fc5c
AJIF7 <joshuafeyi7@gmail.com>
2022-09-22 12:27:16 -0700

    vb

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
  /**
-  * Description: main - Entry point
+  *
+  * main - Entry point
   *
   * Return: Always 0 (Success)
   */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
